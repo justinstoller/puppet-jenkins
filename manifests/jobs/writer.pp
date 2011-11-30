730382c408e8c76a1415640401ee1f477511dd59
Justin Stoller <justin.stoller@gmail.com>
2011-11-30 14:31:58 -0800

    quote possible non-shell safe job names when calling the exec

--- manifests/jobs/writer.pp
+++ manifests/jobs/writer.pp
@@ -19,11 +19,10 @@ define jenkins::jobs::writer ($job_type = "matrix-project") {
 
   exec {
     "write_${title}":
-      command => "/var/lib/jenkins/puppet/xml_builder '/var/lib/jenkins/puppet/${title}/config.xml' ${title} ${job_type}",
+      command => "/var/lib/jenkins/puppet/xml_builder '/var/lib/jenkins/puppet/${title}/config.xml' '${title}' ${job_type}",
       refreshonly => true,
       require => Jenkins::Jobs::Setup["${title}"],
       subscribe => File["/var/lib/jenkins/puppet/${title}"],
       notify => File["/var/lib/jenkins/jobs/${title}/config.xml"],
   }   
-    
 }
